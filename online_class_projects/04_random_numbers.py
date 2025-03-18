0f4dbe0d7aa76427c8b1d137dbc6707a9f7279de
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2025-03-18 08:08:44 +0500

    online_class_projects/04_random_numbers.py

---
+++ online_class_projects/04_random_numbers.py
@@ -0,0 +1,48 @@
+# # # # # # # USING FOR LOOP # # # # # # #
+
+import random
+
+def main() -> None:
+    N_NUMBERS: int = 10
+    MIN_VALUE: int = 1
+    MAX_VALUE: int = 100
+
+    random: int = random_number(MAX_VALUE)
+    num_array: list[int] = []
+    for _ in range(MIN_VALUE, N_NUMBERS):
+        random = random_number(MAX_VALUE)
+        num_array.append(random)
+        print(num_array)
+ 
+def random_number(num: int) -> int:
+    rnd: int = int(random.random() * num)
+    return rnd
+
+if __name__ == '__main__':
+    main()
+
+
+# # # # # # # USING WHILE LOOP # # # # # # #
+
+# import random
+
+# def main() -> None:
+#     N_NUMBERS: int = 10
+#     MIN_VALUE: int = 1
+#     MAX_VALUE: int = 100
+
+#     random: int = random_number(MAX_VALUE)
+#     num_array: list[int] = []
+#     count:int = 1
+#     while count <= N_NUMBERS:
+#         random = random_number(MAX_VALUE)
+#         num_array.append(random)
+#         print(num_array)
+#         count+=1
+ 
+# def random_number(num: int) -> int:
+#     rnd: int = int(random.random() * num)
+#     return rnd
+
+# if __name__ == '__main__':
+#     main()
