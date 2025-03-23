eb26888f1ffd0786e50868bff6b5808fd8bee7f2
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2025-03-24 00:39:49 +0500

    fully made functional with error or bug free

--- online_class_projects/02_intermediate/Control Flow for Console/handout.py
+++ online_class_projects/02_intermediate/Control Flow for Console/handout.py
@@ -39,9 +39,10 @@ def evaluate_guess(random_num: int, guessed_num: int) -> None:
     if random_num == guessed_num:
         result = "Congratulations! You guessed it right! The computer's number was " + str(random_num)
         score += 1
+        random_num = random_number_generator()
     else:
         result = "Sorry! You guessed it wrong."
-
+    print("random number is ", random_num)
     round += 1
 
     if guessed_num > random_num:
@@ -65,70 +66,3 @@ def random_number_generator() -> int:
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-
-
-
-
-
-
-# # from typing import Union
-
-# ROUND: int = 1
-# SCORE: int = 0
-# EXIT: bool = False
-
-# def main() -> None:
-#     print("""
-#         Welcome to the High-Low Game!
-#       --------------------------------
-#     """)
-
-#     high_low()
-
-# def high_low() -> None:
-#     random_number: int = random_number_generator()
-#     while not EXIT:
-#         # your_guessed: Union[int, str] = int(input("Enter your number: "))
-#         your_guessed: str = str(input("Enter your number (Type 'exit' to exit the game):"))
-#         if your_guessed.lower() == 'exit':
-#             EXIT = False
-#             continue
-#         evaluate_guess(random_number, int(your_guessed))
-#     print(random_number)
-
-# def evaluate_guess(random_num: int, guessed_num: int) -> None:
-#     if random_num == guessed_num:
-#         result = "Congratulations! You guessed it right! The computer's number was" + str(random_num)
-#     else:
-#         result = "Sorry! You guessed it wrong."
-
-#     SCORE=+1
-#     ROUND=+1
-
-#     if guessed_num > random_num:
-#         comparison = "higher"
-#     else:
-#         comparison  = "lower"
-
-#     print(f"""
-#     Round {ROUND}
-#     Your number is {guessed_num}
-#     Do you think your number is higher or lower than the computer's?: {comparison}
-#     {result}
-#     Your score is now {SCORE}
-#         """)
-
-
-# def random_number_generator() -> int:
-#     import random
-#     return random.randint(1, 100)
-    
-
-# if __name__ == '__main__':
-#     main()
