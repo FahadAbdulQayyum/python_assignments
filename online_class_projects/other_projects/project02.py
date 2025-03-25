d8f3c6d256e2c11936646095fa3c006c28f36c19
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2025-03-26 01:00:29 +0500

    porject 02 is ready

--- online_class_projects/other_projects/project02.py
+++ online_class_projects/other_projects/project02.py
@@ -1,1 +1,44 @@
 ## Project 2: Guess the Number Game Python Project (computer)
+
+import random
+
+def main() -> None:
+    user_guessing()
+    computer_guessing(10)
+
+def computer_guessing(x: int):
+    low: int = 1
+    high: int = x
+    feedback: str = ''
+    while feedback != 'c':
+        guess: int = random.randint(low, high)
+        feedback = input(f"Is {guess} too high (h), too low (l), or correct (c)?? ").lower()
+        if feedback == 'h':
+            high = guess - 1
+            print("Computer guessed higher!")
+        elif feedback == 'l':
+            low = guess + 1
+            print("Computer guessed lower!")
+    print("Computer guessed it right!")
+
+def user_guessing():
+    random: int = random_number(10)
+    while True:
+        guess: int = int(input("Guess the number: "))
+        guessing_level(random, guess)
+        if random == guess:
+            print("You guessed it right!")
+            break
+
+def random_number(num: int) -> int:
+    rnd: int = int(random.random() * num)
+    return rnd
+
+def guessing_level(random: int, guess: int) -> None:
+    if random < guess:
+        print("You guessed higher!")
+    elif random > guess:
+        print("You guessed lower!")
+
+if __name__ == '__main__':
+    main()
