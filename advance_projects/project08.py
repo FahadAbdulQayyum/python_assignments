68b5ebc205c936b258ed271dcad53b8192f0ba4f
Fahad Abdul Qayyum <fahad.45843@iqra.edu.pk>
2025-04-09 00:42:31 +0500

    completed project08 which is qr

--- advance_projects/project08.py
+++ advance_projects/project08.py
@@ -1,14 +1,33 @@
 ## Project 8: QR code encoder/decoder Python Project
+from datetime import datetime
+import qrcode
+
+from pyzbar.pyzbar import decode
+from PIL import Image
 
 def main():
-    from datetime import datetime
-    print('timestamp..', int(datetime.now().timestamp()))
-    import qrcode
+    timestamp = int(datetime.now().timestamp())
     data = 'Don\'t Give Up, Keep Learning and Growing!'
 
-    img = qrcode.make(data)
+    # simple_qr_code_generator(data, timestamp)
+    # colorful_qr_code_generator(data, timestamp)
+    qr_code_decoder()
 
-    img.save(f"C:/Users/Fahad/Desktop/Practice/python/GSIT/Assignments/advance_projects/qrimage/qr-{int(datetime.now().timestamp())}.png")
+def simple_qr_code_generator(data, timestamp):
+    img = qrcode.make(data)
+    img.save(f"C:/Users/Fahad/Desktop/Practice/python/GSIT/Assignments/advance_projects/qrimage/qr-{timestamp}.png")
+
+def colorful_qr_code_generator(data, timestamp):
+    qr = qrcode.QRCode(version = 1, box_size = 10, border = 5)
+    qr.add_data(data)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color = 'red', back_color = 'white')
+    img.save(f"C:/Users/Fahad/Desktop/Practice/python/GSIT/Assignments/advance_projects/qrimage/colorful-qr-{timestamp}.png")
+
+def qr_code_decoder():
+    img = Image.open(f"C:/Users/Fahad/Desktop/Practice/python/GSIT/Assignments/advance_projects/qrimage/qr-1744140164.png")
+    result = decode(img)
+    print("...result...", result)
 
 if __name__ == '__main__':
     main()
